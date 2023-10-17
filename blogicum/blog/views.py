f652161c2b9e8d59b71e68c19f44c00e054974de
Максим Борисов <wayer555@yandex.ru>
2023-10-17 11:34:00 +0400

    Исправлено

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,25 +1,29 @@
-from django.shortcuts import render, get_object_or_404
-from blog.models import Post
-from blog.models import Category
 from datetime import date
+
+from django.shortcuts import render, get_object_or_404
+
+from blog.models import Post, Category
+
+post_limit = 5
+
+
+def get_posts_qs(model, **filter_fields):
+    return Post.objects.all().filter(
+        is_published=True, category__is_published=True,
+        pub_date__lte=date.today(), **filter_fields)
 
 
 def index(request):
     template = 'blog/index.html'
-    post_list = Post.objects.all().filter(
-        is_published=True, category__is_published=True,
-        pub_date__lte=date.today())[0:5]
-    context = {'post_list': post_list, }
+    posts = get_posts_qs(Post.objects)[:post_limit]
+    context = {'post_list': posts, }
     return render(request, template, context)
 
 
 def post_detail(request, id):
     template = 'blog/detail.html'
     post = get_object_or_404(
-        Post.objects.filter(
-            is_published=True,
-            category__is_published=True,
-            pub_date__lte=date.today()),
+        get_posts_qs(Post.objects),
         id=id)
     context = {'post': post, }
     return render(request, template, context)
@@ -29,8 +33,8 @@ def category_posts(request, category_slug):
     template = 'blog/category.html'
     category = get_object_or_404(
         Category.objects.filter(is_published=True), slug=category_slug)
-    post_list = Post.objects.all().filter(
+    posts = Post.objects.all().filter(
         is_published=True, category__title=category,
         pub_date__lte=date.today())
-    context = {'category': category, 'post_list': post_list, }
+    context = {'category': category, 'post_list': posts, }
     return render(request, template, context)
