97ae94bb25d54f09795785e8a5cc229310c43bf8
Максим Борисов <wayer555@yandex.ru>
2023-10-18 11:09:51 +0400

    Еще больше исправил!_01

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post, Category
 
-post_limit = 5
+POST_LIMIT = 5
 
 
-def get_posts_qs(model, **filter_fields):
+def get_posts_qs(**filter_fields):
     return Post.objects.all().filter(
         is_published=True, category__is_published=True,
         pub_date__lte=date.today(), **filter_fields)
@@ -15,15 +15,15 @@ def get_posts_qs(model, **filter_fields):
 
 def index(request):
     template = 'blog/index.html'
-    posts = get_posts_qs(Post.objects)[:post_limit]
-    context = {'post_list': posts, }
+    posts = get_posts_qs()[:POST_LIMIT]
+    context = {'posts': posts, }
     return render(request, template, context)
 
 
 def post_detail(request, id):
     template = 'blog/detail.html'
     post = get_object_or_404(
-        get_posts_qs(Post.objects),
+        get_posts_qs(),
         id=id)
     context = {'post': post, }
     return render(request, template, context)
@@ -33,8 +33,6 @@ def category_posts(request, category_slug):
     template = 'blog/category.html'
     category = get_object_or_404(
         Category.objects.filter(is_published=True), slug=category_slug)
-    posts = Post.objects.all().filter(
-        is_published=True, category__title=category,
-        pub_date__lte=date.today())
-    context = {'category': category, 'post_list': posts, }
+    posts = get_posts_qs(category__title=category)
+    context = {'category': category, 'posts': posts, }
     return render(request, template, context)
